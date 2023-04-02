c1f89cd64ccbcc1fd15df1a7752e807eff6762e2
javigoo <javierroiggregorio@gmail.com>
2023-04-02 03:27:28 +0200

    pre EJS

--- server/index.js
+++ server/index.js
@@ -8,11 +8,23 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 // Sirve los archivos estáticos de la carpeta "client"
-app.use("/client", express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 // Sirve el archivo index.html en la ruta raíz
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "../client/views/main/index.html"));
+});
+
+app.get("/broadcast", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/views/broadcast/index.html"));
+});
+
+app.get("/canvas", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/views/canvas/index.html"));
+});
+
+app.get("/monitor", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/views/monitor/index.html"));
 });
 
 // Configura el servidor Socket.IO
